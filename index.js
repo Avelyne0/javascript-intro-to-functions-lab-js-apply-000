5178cf4ac8296a4fe4a044e2dac0b773ec82d176
lively-storage-3976 <sam.dbarrie@gmail.com>
2019-04-29 14:13:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   expect(greetSpecialGuest).toEqual(`I can't hear you!`)
   expect(greetSpecialGuest).toEqual(`YES INDEED!`)
-  expect(greetSpecialGuest).toEqual(`I can't hear you!`)
+  expect(greetSpecialGuest).toEqual(`I love you, too.`)
 }
