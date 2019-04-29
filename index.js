32470988a317e98df1f27c9a27921683229e4065
lively-storage-3976 <sam.dbarrie@gmail.com>
2019-04-29 14:19:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
-
-var uppercase = "HELLO!"
-var lowercase = 'hello!'
-var mixedCase = 'Hi there!'
   
 function logShout(string){
   console.log(string.toUpperCase())
@@ -19,9 +15,13 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(string.lowercase) === string){
+  if(string.lowercase() === string){
     return "I can't hear you!"
   }
-  expect(uppercase).toEqual(`YES INDEED!`)
-  expect(`I love you, Grandma.`).toEqual(`I love you, too.`)
+  if(string.uppercase() === string){
+    return "YES INDEED!"
+  }
+  if(string === "I love you, Grandma"){
+    return "I love you, too."
+  }
 }
